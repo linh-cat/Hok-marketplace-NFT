f6a680369ff18604b8817b7b06f9539248ac504a
unknown <linh-cat>
2022-03-19 11:55:56 +0700

    add font

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './index.scss';
 
-
 const index = () => {
 	return (
 		<div className="header">
-			hearder component
+			<div className="header__icon">
+				icon
+			</div>
+			<ul className="header__nav">
+				<li className="header__nav--item">
+					Home
+				</li>
+				<li className="header__nav--item">
+					Hok-Main
+				</li>
+				<li className="header__nav--item">
+					Heroes GenX
+				</li>
+				<li className="header__nav--item">
+					About us
+				</li>
+			</ul>
 		</div>
 	);
 };
