0071f596de176362c27f85ae7f5ae3b076a7bb47
unknown <linh-cat>
2022-04-04 18:26:40 +0700

    change button click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 import ButtonHok from "../ButtonHok"
 import HokLogo from "../../assets/images/HOK-Logo-white.png"
-import {useDispatch , useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import web3 from '../../connection/web3';
 import { loadAccount } from '../../redux/actions/action-creators/actionCreators';
 import { account } from '../../redux/selector/selector';
 
 const Index = () => {
-	const Dispatch = useDispatch()
+	const dispatch = useDispatch()
 	const accountWeb3 = useSelector(account)
-	const connectWalletHandler = async ()=>{
-		let accounts:any 
-        try{
-            await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-        }catch(error) {
-            console.error(error);
-          }
-		if(web3){
-			 accounts = await web3.eth.getAccounts();
+	const [loadingBtn, setLoadingBtn] = useState<boolean>(false);
+
+	const connectWalletHandler = async () => {
+		let accounts: any
+		setLoadingBtn(true);
+		try {
+			await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
+			setLoadingBtn(false);
+		} catch (error) {
+			console.error(error);
+		}
+		if (web3) {
+			accounts = await web3.eth.getAccounts();
 		} else
-		console.log('xxx')
-        const account = accounts[0];
-        Dispatch(loadAccount(account))      
-    }
+			console.log('No account')
+		const account = accounts[0];
+		dispatch(loadAccount(account))
+	}
 	return (
 		<div className="header">
 			<div className="header__icon">
@@ -41,16 +45,13 @@ const Index = () => {
 					<ButtonHok type="link" text="Heroes GenX" color="#E06666" bold='bold' />
 				</li>
 				<li className="header__nav--item">
-					<ButtonHok type="link" text="About us" color="#000000" bold='bold' />
+					<ButtonHok type="link" text="About us" color="#000000" bold='bold'
+					/>
 				</li>
 				<li className="header__nav--item">
-					{/* <ButtonHok  type="default" text="Connect Wallet" bold='bold' border='1px solid #000000' radius='5px' 
-						 onClick={connectWalletHandler}
-					/> */}
-				{accountWeb3 && <h5> Address : {accountWeb3}</h5>}
-				{!accountWeb3 && <button onClick={connectWalletHandler}>Login</button>}
+					{accountWeb3 ? <h5> Address : {accountWeb3}</h5> : <ButtonHok type="default" text="Connect Wallet" bold='bold' border='1px solid #000000' radius='5px' loading={loadingBtn} onClick={connectWalletHandler} />}
 				</li>
-			
+
 			</ul>
 		</div >
 	);
--- src/redux/actions/action-creators/actionCreators.ts
+++ src/redux/actions/action-creators/actionCreators.ts
@@ -1,5 +1,3 @@
-import { Action } from "../action-interfaces/action-interfaces";
-
 export const loadAccount = (account : string) =>{
     return{
         type :'connection/account',
