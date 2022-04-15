958d370f02337c422cad62bb0a02383b541d44d7
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2022-04-15 17:34:12 +0700

    Add modal custome successfully

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './index.scss';
 import ButtonHok from "../ButtonHok"
+import ModalHok from "../ModalHok"
 import ToastHok, { Label, ToastList } from "../ToastHok"
 import HokLogo from "../../assets/images/HOK-Logo-white.png"
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,15 +10,35 @@ import { loadAccount } from '../../redux/actions/action-creators/actionCreators'
 import { account } from '../../redux/selector/selector';
 
 const Index = () => {
+
+
 	const dispatch = useDispatch()
 	const accountWeb3 = useSelector(account)
 	const [loadingBtn, setLoadingBtn] = useState<boolean>(false);
-	const [isLogin, setIsLogin] = useState<boolean>(false);
+
+	const commonModal: {
+		title: string,
+		modalText: string,
+		btnOk: string,
+		btnCancel: string,
+		handleOk: () => void,
+		handleCancel: () => void
+	} = {
+		title: "Confirm",
+		modalText: "this is modal",
+		btnOk: "OK",
+		btnCancel: "Cancel",
+		handleCancel: () => {
+		},
+		handleOk: () => { }
+	}
+
+	const [modalVisible, setModalVisible] = useState<{ visible: boolean, modal: any }>({
+		visible: false,
+		modal: commonModal
+	})
 
 
-	console.log('====================================');
-	console.log(isLogin);
-	console.log('====================================');
 	const connectWalletHandler = async () => {
 		let accounts: any
 		setLoadingBtn(true)
@@ -45,8 +66,23 @@ const Index = () => {
 		}
 	}
 
+	const openModal = () => {
+		const modalHeader = commonModal;
+
+		modalHeader.handleOk = () => {
+			setModalVisible({ visible: false, modal: modalHeader });
+		}
+		modalHeader.handleCancel = () => {
+			setModalVisible({ visible: false, modal: modalHeader });
+
+		}
+		setModalVisible({ visible: true, modal: modalHeader })
+	}
+
 	return (
 		<>
+			<ModalHok title={modalVisible.modal.title} modalText={modalVisible.modal.modalText} visible={modalVisible.visible} handleOk={modalVisible.modal.handleOk} handleCancel={modalVisible.modal.handleCancel} />
+
 			<ToastHok label={Label.SUCCESS} toastList={[{ id: 1, title: "SUCCESS", description: "This is success" }]} position="top-right" show={true} timeShow={3000} />
 
 			<div className="header">
@@ -55,6 +91,9 @@ const Index = () => {
 					<ButtonHok type="link" text="Hok Marketplace" bold='bold' color="#fff" />
 				</div>
 				<ul className="header__nav">
+					<li className="header__nav--item">
+						<ButtonHok type="link" text="Show modal" color="#6FA8DC" bold='bold' onClick={openModal} />
+					</li>
 					<li className="header__nav--item">
 						<ButtonHok type="link" text="Home" color="#6FA8DC" bold='bold' />
 					</li>
--- src/components/ModalHok/index.tsx
+++ src/components/ModalHok/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal } from "antd"
 
-const Index = () => {
+type PropsModal = {
+    visible: boolean,
+    modalText: string,
+    title: string,
+    btnOk?: string,
+    btnCancel?: string,
+    handleOk: () => void,
+    handleCancel: () => void
+}
+
+const Index = ({ visible, modalText, title, handleOk, handleCancel }: PropsModal) => {
+
     return (
-        <div>Index</div>
+        <Modal title={title} visible={visible} onOk={handleOk} onCancel={handleCancel} width="900px" okButtonProps={{ style: { backgroundColor: "rgb(43, 120, 228)" } }}>
+            <p>{modalText}</p>
+        </Modal>
     )
 }
 
