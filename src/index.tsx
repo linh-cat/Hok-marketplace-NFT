76d95215cea7c6530e50f934630b8ce8345ea705
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2022-05-29 21:01:11 +0700

    refactor file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 import 'antd/dist/antd.css';
 import 'react-toastify/dist/ReactToastify.css';
-import store from './redux/store/store'
-import { Provider } from 'react-redux'
+import store from 'redux/store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,16 +1,16 @@
-import { combineReducers } from "redux";
-import connectionReducer from './connectionReducer'
-import collectionReducer from './collectionReducer'
-import marketplaceReducer from './marketplaceReducer'
-import filterReducer from "./filterReducer";
+import { combineReducers } from 'redux';
+import connectionReducer from 'redux/reducer/connectionReducer';
+import collectionReducer from 'redux/reducer/collectionReducer';
+import marketplaceReducer from 'redux/reducer/marketplaceReducer';
+import filterReducer from 'redux/reducer/filterReducer';
 
 const rootReducer = combineReducers({
-    connection: connectionReducer,
-    collection : collectionReducer,
-    marketplace : marketplaceReducer,
-    filter : filterReducer
-})
+	connection: connectionReducer,
+	collection: collectionReducer,
+	marketplace: marketplaceReducer,
+	filter: filterReducer,
+});
 
-export default rootReducer
+export default rootReducer;
 
-export type State = ReturnType<typeof rootReducer>
+export type State = ReturnType<typeof rootReducer>;
